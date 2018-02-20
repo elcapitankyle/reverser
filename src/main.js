c4a26a4c4a4b404dcbe7714f7f14a37295d132cf
kyle.quist <kyle.quist@gmail.com>
2018-02-20 11:21:33 -0700

    Added basic checking of file contents for a number on the first line

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 //Use reverser on any file
-const reverser = require('./reverser'),
-fs = require('fs');
+const reverser = require('./reverser');
 var fileToTest = '';
 
 function loadTheFile(fileName){
-    //load the file
-    //let fs = require('fs');
-    console.log('Starting loadTheFile');
+    let fs = require('fs');
     var rawdata = fs.readFileSync(fileName);
     var linesOfFile = rawdata.toString().split("\n"); //Convert to an array of strings
     return linesOfFile;
@@ -18,15 +15,20 @@ if(process.argv.length != 3){
     process.exit(0);
 }else{
     fileToTest = process.argv[2];
-    cosole.log('calling loadTheFile function')
+
     console.log(`Reversing all lines of text in ${fileToTest}`);
     var linesOfFile = loadTheFile(fileToTest);
-    console.log('After loadTheFile function')
-    console.log('File contents: ' + linesOfFile);
-    for(var i = 1; i <= linesOfFile[0];i++){
-        let reversedLine = reverser.reverse(linesOfFile[i]);
-        console.log(`Case#${i}: ${reversedLine}`);
+    
+    if(isNaN(+linesOfFile[0])){
+        console.log("Missing a number as the first line of the file. Please add a number as the first line of the file representing the number of lines that follow. Exiting now with status 'very disappointed in your trickery.")
+        process.exit(0);
+    }else{
+        for(var i = 1; i <= linesOfFile[0];i++){
+            let reversedLine = reverser.reverse(linesOfFile[i]);
+            console.log(`Case#${i}: ${reversedLine}`);
+        }
     }
+    
 };
 
 
