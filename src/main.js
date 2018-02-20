bdb941884be0a83ebc68efa7c97a72bcd0f35f3c
kyle.quist <kyle.quist@gmail.com>
2018-02-20 10:16:34 -0700

    update to code order

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 //Use reverser on any file
-const reverser = require('./reverser');
+const reverser = require('./reverser'),
+fs = require('fs');
 var fileToTest = '';
+
+function loadTheFile(fileName){
+    //load the file
+    //let fs = require('fs');
+    console.log('Starting loadTheFile');
+    var rawdata = fs.readFileSync(fileName);
+    var linesOfFile = rawdata.toString().split("\n"); //Convert to an array of strings
+    return linesOfFile;
+};
 
 if(process.argv.length != 3){
     console.log('Expected usage: node main.js anyInputFileName');
@@ -9,23 +19,17 @@ if(process.argv.length != 3){
 }else{
     fileToTest = process.argv[2];
     console.log(`Reversing all lines of text in ${fileToTest}`);
-};
-
-function loadTheFile(fileName){
-    //load the file
-    let fs = require('fs');
-    var rawdata = fs.readFileSync(fileName);
     
-    var linesOfFile = rawdata.toString().split("\n"); //Convert to an array of strings
-    return linesOfFile;
+    var linesOfFile = loadTheFile(fileToTest);
+    
+    console.log('File contents: ' + linesOfFile);
+    for(var i = 1; i <= linesOfFile[0];i++){
+        let reversedLine = reverser.reverse(linesOfFile[i]);
+        console.log(`Case#${i}: ${reversedLine}`);
+    }
 };
 
-var linesOfFile = loadTheFile(fileToTest);
 
-for(var i = 1; i <= linesOfFile[0];i++){
-    let reversedLine = reverser.reverse(linesOfFile[i]);
-    console.log(`Case#${i}: ${reversedLine}`);
-}
 
 
 
